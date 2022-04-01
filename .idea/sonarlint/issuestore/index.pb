5042dced0272459ce4530c0818ab4e73866020e0
Simeon.Andreev <simeon.zlatanov.andreev@gmail.com>
2022-04-01 19:50:45 +0300

    P2

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+g
+7mobile/src/main/java/bg/o/sim/dropplett/MainActivity.kt,3\4\34a018e0e4e1d47c91bd6f398349fd76151c3012
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+\
+,mobile/src/main/res/layout/activity_main.xml,e\d\ed26169ec03542d6e198285f535f4eb67cc535e5
--- wear/src/main/java/bg/o/sim/dropplett/MainActivity.kt
+++ wear/src/main/java/bg/o/sim/dropplett/MainActivity.kt
@@ -1,5 +1,6 @@
 package bg.o.sim.dropplett
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.os.Bundle
 import bg.o.sim.dropplett.databinding.ActivityMainBinding
@@ -11,85 +12,96 @@ class MainActivity : Activity() {
     private var green: Byte = 20
     private var white: Byte = 20
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.clickGrid0.apply {
-            add.setOnClickListener {
-                red = red.inc()
-                binding.count0.text = "$red"
+        binding.count0.setOnTouchListener(CompoundDrawableTouchListener{
+            when(it){
+                CompoundDrawableTouchListener.LEFT ->{
+                    red = red.dec()
+                    binding.count0.text = "$red"
+                }
+                CompoundDrawableTouchListener.TOP ->{
+                    red = 0
+                    binding.count0.text = ""
+                }
+                CompoundDrawableTouchListener.RIGHT ->{
+                    red = red.inc()
+                    binding.count0.text = "$red"
+                    
+                }
+                CompoundDrawableTouchListener.BOTTOM ->{
+                    red = 20
+                    binding.count0.text = "$blue"
+                }
             }
-            subtract.setOnClickListener {
-                red = red.dec()
-                binding.count0.text = "$red"
+        })
+        binding.count1.setOnTouchListener(CompoundDrawableTouchListener{
+            when(it){
+                CompoundDrawableTouchListener.LEFT ->{
+                    blue = blue.dec()
+                    binding.count1.text = "$blue"
+                }
+                CompoundDrawableTouchListener.TOP ->{
+                    blue = 0
+                    binding.count1.text = ""
+                }
+                CompoundDrawableTouchListener.RIGHT ->{
+                    blue = blue.inc()
+                    binding.count1.text = "$blue"
+                    
+                }
+                CompoundDrawableTouchListener.BOTTOM ->{
+                    blue = 20
+                    binding.count1.text = "$blue"
+                }
             }
-            reset.setOnClickListener {
-                red = 20
-                binding.count0.text = "$red"
+        })
+        binding.count2.setOnTouchListener(CompoundDrawableTouchListener{
+            when(it){
+                CompoundDrawableTouchListener.LEFT ->{
+                    green = green.dec()
+                    binding.count2.text = "$green"
+                }
+                CompoundDrawableTouchListener.TOP ->{
+                    green = 0
+                    binding.count2.text = ""
+                }
+                CompoundDrawableTouchListener.RIGHT ->{
+                    green = green.inc()
+                    binding.count2.text = "$green"
+                    
+                }
+                CompoundDrawableTouchListener.BOTTOM ->{
+                    green = 20
+                    binding.count2.text = "$green"
+                }
             }
-            droid.setOnClickListener {
-                red = 0
-                binding.count0.text = ""
+        })
+        binding.count3.setOnTouchListener(CompoundDrawableTouchListener{
+            when(it){
+                CompoundDrawableTouchListener.LEFT ->{
+                    white = white.dec()
+                    binding.count3.text = "$white"
+                }
+                CompoundDrawableTouchListener.TOP ->{
+                    white = 0
+                    binding.count3.text = ""
+                }
+                CompoundDrawableTouchListener.RIGHT ->{
+                    white = white.inc()
+                    binding.count3.text = "$white"
+                    
+                }
+                CompoundDrawableTouchListener.BOTTOM ->{
+                    white = 20
+                    binding.count3.text = "$white"
+                }
             }
-        }
-
-
-        binding.clickGrid1.apply {
-            add.setOnClickListener {
-                blue = blue.inc()
-                binding.count1.text = "$blue"
-            }
-            subtract.setOnClickListener {
-                blue = blue.dec()
-                binding.count1.text = "$blue"
-            }
-            reset.setOnClickListener {
-                blue = 20
-                binding.count1.text = "$blue"
-            }
-            droid.setOnClickListener {
-                blue = 20
-                binding.count1.text = ""
-            }
-        }
-        binding.clickGrid2.apply {
-            add.setOnClickListener {
-                green = green.inc()
-                binding.count2.text = "$green"
-            }
-            subtract.setOnClickListener {
-                green = green.dec()
-                binding.count2.text = "$green"
-            }
-            reset.setOnClickListener {
-                green = 20
-                binding.count2.text = "$green"
-            }
-            droid.setOnClickListener {
-                green = 20
-                binding.count2.text = ""
-            }
-        }
-        binding.clickGrid3.apply {
-            add.setOnClickListener {
-                blue = blue.inc()
-                binding.count3.text = "$blue"
-            }
-            subtract.setOnClickListener {
-                blue = blue.dec()
-                binding.count3.text = "$blue"
-            }
-            reset.setOnClickListener {
-                blue = 20
-                binding.count3.text = "$blue"
-            }
-            droid.setOnClickListener {
-                blue = 20
-                binding.count3.text = ""
-            }
-        }
+        })
     }
 }
